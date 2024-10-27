6db263187fe5722afa6671db8ce477fe658b2862
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-27 12:01:19 -0400

    1

---
+++ Assignment01/MovieShop.WebMVC/MovieShopDbApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MovieShopDbApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    
+}
