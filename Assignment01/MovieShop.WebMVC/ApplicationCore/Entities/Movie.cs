a4f3181d8702a90112ca2121cdfa4cbfcf04b3f4
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-27 03:59:34 -0400

    2

---
+++ Assignment01/MovieShop.WebMVC/ApplicationCore/Entities/Movie.cs
@@ -0,0 +1,10 @@
+namespace ApplicationCore.Entities;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Overview { get; set; }
+    public string? PostUrl{get; set;}
+    
+}
