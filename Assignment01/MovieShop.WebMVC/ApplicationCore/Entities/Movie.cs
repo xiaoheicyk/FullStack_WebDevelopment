b3c2a470ce09a60693346a5f7d357b3482dc0c29
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-27 07:28:19 -0400

    Update Movie.cs

--- Assignment01/MovieShop.WebMVC/ApplicationCore/Entities/Movie.cs
+++ Assignment01/MovieShop.WebMVC/ApplicationCore/Entities/Movie.cs
@@ -1,12 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ApplicationCore.Entities;
 
 public class Movie
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
-    public string? Overview { get; set; }
-    public string? PostUrl{get; set;}
+    public string? BackdropUrl { get; set; }
+    public decimal Budget { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public string? ImdbUrl { get; set; }
+    public string? OriginalLanguage { get; set; }
+    public decimal Price { get; set; }
     public DateTime ReleaseDate { get; set; }
+    public decimal Revenue { get; set; }
+    public int Runtime { get; set; }
+    [Column()]
+    public string? Tagline { get; set; }
     
     
 }
