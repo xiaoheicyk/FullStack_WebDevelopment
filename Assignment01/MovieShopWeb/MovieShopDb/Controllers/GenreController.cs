a66baa8d11c62f6d5dc8065a2dc152e70314f864
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-28 10:32:41 -0400

    3

--- Assignment01/MovieShopWeb/MovieShopDb/Controllers/GenreController.cs
+++ Assignment01/MovieShopWeb/MovieShopDb/Controllers/GenreController.cs
@@ -23,5 +23,54 @@ public class GenreController : Controller
     {
         return View();
     }
+
+    [HttpPost]
+    public IActionResult Create(Genre genre)
+    {
+        if (ModelState.IsValid)
+        {
+            _genreService.AddGenre(genre);
+            return RedirectToAction("Index");
+        }
+        return View(genre);
+    }
+
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var result = _genreService.GetGenreById(id);
+        return View(result);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Genre genre)
+    {
+        if (ModelState.IsValid)
+        {
+            _genreService.UpdateGenre(genre, genre.Id);
+            return RedirectToAction("Index");
+        }
+        return View(genre);
+    }
+
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var result = _genreService.GetGenreById(id);
+        return View(result);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Genre genre)
+    {
+        if (ModelState.IsValid)
+        {
+            _genreService.DeleteGenre(genre.Id);
+            return RedirectToAction("Index");
+        }
+        return View(genre);
+    }
+    
+    
     
 }
--- Assignment01/MovieShopWeb/MovieShopDb/obj/Debug/net8.0/MovieShopDb.AssemblyInfo.cs
+++ Assignment01/MovieShopWeb/MovieShopDb/obj/Debug/net8.0/MovieShopDb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieShopDb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26438d30d03cd846c2657fa7e9f6ecadedc223d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ab895782e1f0bcfd069f6f7227af2b2eb7f6c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieShopDb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieShopDb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
