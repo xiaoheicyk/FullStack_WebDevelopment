be845cf5c82366a0c36f875458179cd66b1a2c74
xiaoheicyk <57608906+xiaoheicyk@users.noreply.github.com>
2024-10-30 16:30:18 -0400

    1

---
+++ Assignment01/MovieShopWebShop.MVC/ApplicationCore/Contracts/Models/MovieCardModel.cs
@@ -0,0 +1,15 @@
+namespace ApplicationCore.Contracts.Models;
+
+public class MovieCardModel
+{
+    public int Id { get; set; }
+    
+    public string? Title { get; set; }
+    
+    public string? Overview { get; set; }
+    
+    public string? BackdropPath { get; set; }
+    
+    public string? ImbdUrl { get; set; }
+    
+}
